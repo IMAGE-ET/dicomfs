25b7e3964cfa5ff5983358276a2466ced1015167
david.volgyes <david.volgyes@gmail.com>
2012-09-12 11:53:31 +0000

    [change] some style and encoding change.

--- source/dicomfs.py
+++ source/dicomfs.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python2.7
+# -*- coding: utf8 -*- 
+
 #
+#     Copyright 2010 - 2012 David Volgyes
 #
-#    This program can be distributed under the terms of the GNU LGPL
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public License 
+#    along with this program.  If not, see <https://www.gnu.org/licenses/lgpl.html>.
 #
 
 import os, stat, errno, commands, sys, re,time,threading
